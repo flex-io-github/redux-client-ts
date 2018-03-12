474ee22f7240e3d37b1c264c9ff09b507980195a
Ronald Manzano <ngnl_rmanzano@hotmail.com>
2018-03-12 11:57:21 +1100

    update footer

--- src/layouts/components/layout-footer.tsx
+++ src/layouts/components/layout-footer.tsx
@@ -4,12 +4,8 @@ import '../_styles/Footer.css'
 
 const Footer = () => (
   <div className="footer-container">
-    {'© Awesome App 2016. '}
-    <Link href="https://github.com/guzmonne/office-ui-layout/tree/01-blank-project">Get in touch!</Link>
-    {' -- Made with '}
-    <span className="text-red">♥</span>
-    {' by '}
-    <Link href="https://github.com/guzmonne">Guzmán Monné {'<@guzmonne>'}</Link>
+    {'© Awesome App 2018. '}
+
   </div>
 )
 
